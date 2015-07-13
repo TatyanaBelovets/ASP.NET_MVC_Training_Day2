05eb35de38f41430c004aae1d4614cf9a7eb2501
TatyanaBelovets <Tatyana.Belovets@gmail.com>
2015-07-13 13:03:30 +0300

    Added GCD functions and tests

---
+++ Task2.Library/HexNumberFormat.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+
+namespace Task2.Library
+{
+    public class HexNumberFormat : IFormatProvider, ICustomFormatter
+    {
+        private static readonly Dictionary<int, char> Conversions =
+            Enumerable.Range(0, 0xf + 1).ToDictionary(x => x, x => x.ToString("X")[0]);
+
+        public object GetFormat(Type formatType)
+        {
+            return formatType == typeof(ICustomFormatter) ? this : null;
+        }
+
+        public string Format(string format, object arg, IFormatProvider formatProvider)
+        {
+            string upperFormat = format.ToUpper(CultureInfo.InvariantCulture); 
+            if (arg.GetType() != typeof(int) || upperFormat != "HEX")
+                try
+                {
+                    return HandleOtherFormats(format, arg);
+                }
+                catch (FormatException e)
+                {
+                    throw new FormatException(String.Format("The format of '{0}' is invalid.", format), e);
+                }
+            var number = (int) arg;
+            var result = new StringBuilder();
+            for (var tmp = Math.Abs(number); tmp != 0; tmp >>= 4)
+            {
+                result.Insert(0, Conversions[tmp & 0xF]); //& 0xF - take last 4 bytes of the number
+            }
+            result.Insert(0, "0x");
+            result.Insert(0, (number < 0) ? "-" : ""); // insert sign of the number
+            return result.ToString();
+        }
+
+        private static string HandleOtherFormats(string format, object arg)
+        {
+            var formattable = arg as IFormattable;
+            if (formattable != null)
+                return formattable.ToString(format, CultureInfo.CurrentCulture);
+            return arg != null ? arg.ToString() : String.Empty;
+        }
+    }
+}
---
+++ Task3.Library/GreatestCommonDivisor.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3.Library
+{
+    public static class GreatestCommonDivisor
+    {
+        private static int Gcd(int a, int b)
+        {
+            while (true)
+            {
+                if (b == 0) return a;
+                var temp = a;
+                a = b;
+                b = temp % b;
+            }
+        }
+
+
+        public static int GcdWithTime(out long timeOfWork, int a, params int[] rest)
+        {
+            var watch = Stopwatch.StartNew();
+            var gcdResult = rest.Aggregate(a, Gcd);
+            watch.Stop();
+            timeOfWork = watch.ElapsedMilliseconds;
+            return gcdResult;
+        }
+
+        public static uint GcdByStein(uint u, uint v)
+        {
+            if (u == v || v == 0)
+                return u;
+            if (u == 0)
+                return v;
+            if ((u & 1) == 0) // u is even
+            {
+                if ((v & 1) == 1) // v is odd
+                    return GcdByStein(u >> 1, v);
+                return GcdByStein(u >> 1, v >> 1) << 1; // both u and v are even   
+            }
+            if ((v & 1) == 0) // u is odd, v is even
+                return GcdByStein(u, v >> 1);
+            return u > v ? GcdByStein((u - v) >> 1, v) : GcdByStein((v - u) >> 1, u); // reduce larger argument
+        }
+
+        public static int GcdBySteinsAndTimeOfWork(out long workTime, int a, params int[] rest)
+        {
+            var watch = Stopwatch.StartNew();
+            var gcdResult = rest.Select(i => (uint)Math.Abs(i)).Aggregate((uint)Math.Abs(a), GcdByStein);
+            watch.Stop();
+            workTime = watch.ElapsedMilliseconds;
+            return (int)gcdResult;
+        }
+    }
+}
